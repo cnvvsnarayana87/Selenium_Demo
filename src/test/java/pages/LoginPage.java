fe4c1fa4e7dc216f6eb1261e90a0c4f94fe39602
V V Satyanarayana Nallam <cnvvsnarayana87@gmail.com>
2024-04-06 12:37:30 +0530

    Selenium Project

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -6,25 +6,29 @@ import org.openqa.selenium.WebElement;
 
 public class LoginPage {
 	private WebDriver driver;
+	private By pageHeader = By.tagName("h2");
+	private By userNameField = By.id("username");
+	private By passwordField = By.id("password");
+	private By loginBtn = By.className("radius");
 
 	public LoginPage(WebDriver driver) {
 		this.driver = driver;
 	}
 
 	public String pageHeader() {
-		return this.driver.findElement(By.tagName("h2")).getText();
+		return this.driver.findElement(pageHeader).getText();
 	}
 
 	private WebElement userNameField() {
-		return this.driver.findElement(By.id("username"));
+		return this.driver.findElement(userNameField);
 	}
 
 	private WebElement passwordField() {
-		return this.driver.findElement(By.id("password"));
+		return this.driver.findElement(passwordField);
 	}
 
 	private WebElement loginBtn() {
-		return this.driver.findElement(By.className("radius"));
+		return this.driver.findElement(loginBtn);
 	}
 
 	public SecureAreaPage performLogin(String userName, String password) {
